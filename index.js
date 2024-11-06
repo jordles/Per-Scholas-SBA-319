6b0d2f85fa0193d0115cd76d368c134a237b4d37
Jacky Yang <jackyyang1670@gmail.com>
2024-11-06 02:41:46 -0800

    Updates files

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-import express from 'express';
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import grades from './routes/grades.js';
+import express from 'express'; //routes
+import usersRouter from "./routes/users.js";
+import loginsRouter from "./routes/logins.js";
+import postsRouter from "./routes/posts.js";
+
+
+import 'dotenv/config'; //environment
+import mongoose from 'mongoose'; //models
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //parsing request bodies
+app.use(express.urlencoded({ extended: true })); //parsing form data
+
+app.use("/users", usersRouter); // set up routes
+app.use("/logins", loginsRouter);
+app.use("/posts", postsRouter);
+
+
+try{ //test our mongoose connection
+  mongoose.connect(process.env.ATLAS_URI);
+  console.log("Mongoose connected to the database successfully!");
+} catch (err) {
+  console.error("Mongoose connection error:", err);
+}
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  console.error(err);
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Listen for PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}.`)
+})
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import UserController from '../controllers/users.js';
+
+const router = express.Router();
+
+export default router
